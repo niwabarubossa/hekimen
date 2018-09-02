808e1c5528c6a3a6b39c4a973c5330b51ae13cef
ryogo.niwa <niwabarubossa@yahoo.co.jp>
2018-09-02 08:55:24 +0000

    user login ver1.0

---
+++ db/migrate/20180902023113_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :nickname
+      t.string :email
+      t.string :password_digest
+      t.string :realname
+      t.string :postalcode
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
